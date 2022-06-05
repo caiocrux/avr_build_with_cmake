3fbf9615df956f3f4ffea792785b39204ea4d65e
Caio Felipe Cruz <ccruz@daitan.com>
2022-06-05 14:41:16 -0400

    running test to check if usart is working

--- basic_example/src/main.c
+++ basic_example/src/main.c
@@ -1,15 +1,17 @@
-
 /* Example of CMake usage with AVR. Shows how to add lib directories and  include in main program */
 #include <avr/io.h>
-#include "blink.h"
-#include "wait.h"
+#include <blink.h>
+#include <wait.h>
+#include <usart.h>
 
-int main(void)
-{
+int main(void) {
+    USART0_Init(E_BAUD_1000000);
+    USART0_SendString("Hello World !!\r\n");
 	/* Sets PORT B.5 as an output */
     DDRB |= (1 << PB5);
     while(1){
         blink();
         wait();
+        USART0_SendByte(USART0_ReceiveByte());
     }
 }
