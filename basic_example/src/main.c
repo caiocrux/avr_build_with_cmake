0e949500e137f2e2d7d47409509a07df0965da03
Caio Felipe Cruz <ccruz@daitan.com>
2022-06-20 21:59:40 -0400

    added the class power led to test

--- basic_example/src/main.c
+++ basic_example/src/main.c
@@ -5,13 +5,20 @@
 #include <usart.h>
 #include <led.h>
 #include <gpio.h>
+#include <powerled.h>
 
 int main(void) {
     //GPIO_Init();
     USART0_Init(E_BAUD_1000000);
+    
     Led_Type arduino_led = {0};
+    PowerLed_Type ard_led_power = {0};
+
     Led(&arduino_led,GREEN, OFF);
+    PowerLed(&ard_led_power, GREEN, OFF, CURR_NORMAL, DIAM_5MM, VOL_NORMAL);
     USART0_SendString("Hello World !!\r\n");
+    USART0_SendString("Active Power is :");
+    USART0_SendInterger(PowerLed_ComputePower(&ard_led_power));
     while(1) {
         LED_setState(&arduino_led, ON);
         if(LED_getState(&arduino_led) == ON) {
